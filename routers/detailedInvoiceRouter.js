c04a9663ab0c75773bb2168812f81ba5c657fd45
HoangChien201 <hoangchien11522@gmail.com>
2023-07-29 00:12:10 +0700

    test

--- routers/detailedInvoiceRouter.js
+++ routers/detailedInvoiceRouter.js
@@ -5,7 +5,7 @@ const router=require('express').Router();
 router.get('/unprocessed',detailedInvoiceController.getUnprocessed);
 router.get('/:id',detailedInvoiceController.getAllOfBill);
 router.post('/',detailedInvoiceController.addDetailedInvoice);
-router.put('/:id',detailedInvoiceController.updateDetailedInvoice);
+router.put('/update/:id',detailedInvoiceController.updateDetailedInvoice);
 router.delete('/:id',detailedInvoiceController.deleteDetailedInvoice);
 
 module.exports=router;
