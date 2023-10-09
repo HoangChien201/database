3575b939563fe59d18030d308128c039a37621ef
HoangChien201 <hoangchien11522@gmail.com>
2023-10-09 10:27:23 +0700

    update search

--- routers/newsRouter.js
+++ routers/newsRouter.js
@@ -3,6 +3,8 @@ const router=require('express').Router();
 
 router.get("/",newsController.getAllNews);
 router.get("/:id",newsController.getANews);
+router.get("/search/:key",newsController.searchNews);
+
 
 router.post("/",newsController.addNews);
 router.delete("/:id",newsController.deleteNews);
